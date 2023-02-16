05c25639d0ce007688b35687818ce1cdc7f96206
IamThe-oS <tmagpantay@up.edu.ph>
2023-02-16 09:21:45 +0800

    fixed dbURL for development

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
 
 // const dbUrl = process.env.DB_URL
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp"
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp"
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
